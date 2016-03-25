3fca8af3861eb6a5509ed7505f15d1666122fc4e
hitzi <github@internetists.de>
2016-03-26 00:58:50 +0100

    Update manage.py
    remove update execute_manager(settings)

--- www/manage.py
+++ www/manage.py
@@ -31,4 +31,6 @@ except ImportError:
     sys.exit(1)
 
 if __name__ == "__main__":
-    execute_manager(settings)
+    from django.core.management import execute_from_command_line
+
+        execute_from_command_line(sys.argv)
